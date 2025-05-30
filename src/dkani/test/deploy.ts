6ca3b25748fed8ee4a5adaf2eed6ad22ea4ad849
Dariusz Kanicki <github@dariusz.kanicki.net>
2025-05-30 15:58:21 +0200

    #3 - dev/test environment

---
+++ src/dkani/test/deploy.ts
@@ -0,0 +1,52 @@
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { rmSync, mkdirSync, readFile } from "fs";
+import { cp, copyFile } from "fs/promises";
+
+interface Config {
+    testVault: string;
+}
+
+function _loadConfig(): Config {
+    const data = fs.readFileSync("src/dkani/test/config.json", "utf8");
+    return JSON.parse(data) as Config;
+}
+
+export async function deploy() {
+    const { testVault } = _loadConfig();
+    const pluginDir = `${testVault}/.obsidian/plugins/obsidian-git`;
+
+    mkdirSync("dist", { recursive: true });
+
+    const assets = [
+        ["main.js", "dist/main.js"],
+        ["manifest.json", "dist/manifest.json"],
+        ["styles.css", "dist/styles.css"],
+    ];
+    assets.push(["dkani/config/obsidian_askpass.sh", "dist/obsidian_askpass.sh"]);
+    assets.push(["dkani/config/data.json", "dist/data.json"]);
+
+    await Promise.all(assets.map(([src, dest]) => copyFile(src, dest)));
+
+    rmSync(pluginDir, { recursive: true, force: true });
+    mkdirSync(pluginDir, { recursive: true });
+    await _deployFolder("dist", pluginDir);
+    const now = new Date().toLocaleString();
+    console.log(`✅ Deployment completed. [${now}]`);
+}
+
+async function _deployFolder(sourceDir: string, targetDir: string) {
+    console.log("deploying ...");
+    console.log(`from: './${sourceDir}'`);
+    console.log(`to  : ${targetDir}:`);
+    await cp(sourceDir, targetDir, {
+        recursive: true,
+        filter: (source) => {
+            const stat = fs.statSync(source);
+            if (stat.isFile()) {
+                console.log(`      ${path.basename(source)}`);
+            }
+            return true;
+        },
+    });
+}
